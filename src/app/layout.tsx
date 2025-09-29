f15b95d269aa5cb62a4f290808d2c4a5afd9bb85
Аброс Нахатакян <a.nakhatakyan@dats.team>
2025-09-29 14:34:52 +0300

    feat: Add Yandex.Metrika tracking script for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import {
   organizationJsonLd,
   movieStreamingServiceJsonLd,
 } from "@/lib/seo";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -139,6 +140,30 @@ export default function RootLayout({
         <Navigation />
         <main>{children}</main>
         <Footer />
+
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t);a=e.getElementsByTagName(t)[0];k.async=1;k.src=r;a.parentNode.insertBefore(k,a);
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104316810', 'ym');
+
+            ym(104316810, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/104316810"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
       </body>
     </html>
   );
